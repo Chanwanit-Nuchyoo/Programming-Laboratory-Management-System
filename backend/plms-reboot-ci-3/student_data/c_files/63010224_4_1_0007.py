1bee76845347678056e5ebb36944a1955238974e
CHANWANIT NUCHYOO <63010224@kmitl.ac.th>
2024-02-20 16:27:43 +0700

    Change mergeview bg color

---
+++ backend/plms-reboot-ci-3/student_data/c_files/63010224_4_1_0007.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n < 2:
+        return str(n)
+    else:
+        return factorial(n-1) + "*" + str(n)
+
+print(" *** Factorial ***")
+n = int(input("Enter an integer(n) : "))
+result = factorial(n)
+print(f"Fac({n}) => {result} = {eval(result)}")
