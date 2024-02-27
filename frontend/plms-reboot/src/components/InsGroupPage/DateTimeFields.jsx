eefd2e3de494e480312802f20b3f9fd0ee57a9d2
CHANWANIT NUCHYOO <63010224@kmitl.ac.th>
2024-02-28 02:18:34 +0700

    Add server time display in DateTimeFields component

--- frontend/plms-reboot/src/components/InsGroupPage/DateTimeFields.jsx
+++ frontend/plms-reboot/src/components/InsGroupPage/DateTimeFields.jsx
@@ -1,22 +1,31 @@
 /* eslint-disable react/prop-types */
-import { Stack, Paper } from '@mui/material'
+import { Stack, Paper, Typography, Box } from '@mui/material'
 /* import { DateTimeField } from '@mui/x-date-pickers' */
 import { Controller } from 'react-hook-form'
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { serverTimeOffsetAtom } from "@/store/store";
 import { useAtom } from "jotai";
+import useCurrentTime from "@/hooks/useCurrentTime";
 import moment from 'moment';
 
 const DateTimeFields = ({ prefix, control }) => {
   const [serverTimeOffset, setServerTimeOffset] = useAtom(serverTimeOffsetAtom);
+  const currentTime = useCurrentTime();
 
   return (
     <Paper sx={{
       display: 'flex',
+      flexDirection: 'column',
       padding: '20px',
+      gap: '20px',
       justifyContent: "center",
-      marginTop: "10px",
+      marginTop: "20px",
     }} >
+      <Stack direction="row" justifyContent="center" alignItems="center" >
+        <Box width="320px" padding="10px 10px 10px 12px" bgcolor="black" borderRadius="8px" whiteSpace="nowrap" >
+          <Typography >Server Time: {currentTime.format('Do-MMM-YYYY HH:mm:ss')}</Typography>
+        </Box>
+      </Stack>
       <Stack direction="row" spacing={"10px"} alignItems="center">
         <Controller
           name={`${prefix}_time_start`}
