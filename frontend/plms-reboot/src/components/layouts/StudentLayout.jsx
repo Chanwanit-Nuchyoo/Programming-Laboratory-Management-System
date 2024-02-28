ccf487a6f82eeff2b8a2021e55c0a64ceb0c3276
CHANWANIT NUCHYOO <63010224@kmitl.ac.th>
2024-02-28 22:39:54 +0700

    Add ServerTimeDisplay component to StudentLayout

--- frontend/plms-reboot/src/components/layouts/StudentLayout.jsx
+++ frontend/plms-reboot/src/components/layouts/StudentLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { Box } from "@mui/system";
+import ServerTimeDisplay from "@/components/_shared/ServerTimeDisplay";
 import Sidebar from "@/components/_shared/Sidebar";
 import UserAvatar from "@/components/_shared/UserAvatar"
-import { Box } from "@mui/system";
 
 const RootLayout = () => {
   return (
     <>
       <Sidebar />
       <Box marginLeft={10} paddingX={4} paddingY={2} minHeight={"100vh"} position={"relative"} >
+        <ServerTimeDisplay />
         <UserAvatar />
         <Box paddingTop={"10px"} >
           <Outlet />
