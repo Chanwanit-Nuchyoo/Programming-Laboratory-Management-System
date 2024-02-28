d7e0c3e98f7bf3493ca59a8763fa6cb1763f0708
CHANWANIT NUCHYOO <63010224@kmitl.ac.th>
2024-02-28 22:39:54 +0700

    Add ServerTimeDisplay component to StudentLayout

--- frontend/plms-reboot/src/components/_shared/ServerTimeDisplay.jsx
+++ frontend/plms-reboot/src/components/_shared/ServerTimeDisplay.jsx
@@ -4,7 +4,7 @@ import useCurrentTime from "@/hooks/useCurrentTime";
 const ServerTimeDisplay = () => {
   const currentTime = useCurrentTime();
   return (
-    <Box position="fixed" width="320px" bottom={0} right="0px" padding="5px 10px" bgcolor="black" borderRadius="8px 0px 0px 0px" style={{ whiteSpace: 'nowrap' }}>
+    <Box position="fixed" width="320px" bottom={0} right="0px" padding="5px 10px" bgcolor="black" borderRadius="8px 0px 0px 0px" style={{ whiteSpace: 'nowrap', zIndex: "100" }}>
       Server Time: {currentTime.format('Do-MMM-YYYY HH:mm:ss')}
     </Box>
   )
