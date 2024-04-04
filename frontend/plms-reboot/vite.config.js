74792e25f19116fc21a004990d53d2504ff57ef3
Chanwanit Nuchyoo <peaknuchyoo@gmail.com>
2024-04-05 05:34:10 +0700

    roll back before using gzip

--- frontend/plms-reboot/vite.config.js
+++ frontend/plms-reboot/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
     react(),
-    viteCompression({ algorithm: 'gzip' }) // Enable gzip compression
   ],
   resolve: {
     alias: {
@@ -22,8 +20,6 @@ export default defineConfig({
   },
   build: {
     minify: true,
-    outDir: 'dist', // Specify the output directory
-    assetsDir: 'assets', // Specify the directory to nest assets under
     sourcemap: false, // Disable sourcemaps for production
     brotliSize: false, // Disable size reports for brotli compression
   },
