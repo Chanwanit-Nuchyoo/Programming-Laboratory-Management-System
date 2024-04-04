21874191673e3b95856373e86b993b0d9f18b883
Chanwanit Nuchyoo <peaknuchyoo@gmail.com>
2024-04-05 06:10:32 +0700

    enable gzip support

--- frontend/plms-reboot/vite.config.js
+++ frontend/plms-reboot/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { compression } from 'vite-plugin-compression2'
 
 export default defineConfig({
   plugins: [
     react(),
+    compression()
   ],
   resolve: {
     alias: {
